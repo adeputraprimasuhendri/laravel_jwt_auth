1127b8e7228e2a403ed873f6e7cbf5cf9438d0bb
adeputraprimasuhendri <adeputra.mtech@gmail.com>
2020-07-10 10:52:43 +0700

    CONTROLLER

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-    //
+    public function index(Request $request){
+        return 'DAFTAR';
+    }
+    public function detail($id){
+        return 'DETAIL :: '.$id;
+    }
+    public function create(Request $request){
+        return 'CREATE';
+    }
+    public function update(Request $request){
+        return 'UPDATE';
+    }
+    public function delete(Request $request){
+        return 'DELETE';
+    }
 }
