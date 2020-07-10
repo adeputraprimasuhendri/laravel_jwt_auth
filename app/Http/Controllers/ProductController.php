5a8e7321b960f17d631299c545d03b7a30b430d2
adeputraprimasuhendri <adeputra.mtech@gmail.com>
2020-07-10 10:43:55 +0700

    CONTROLLER

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(Request $request){
+        return 'DAFTAR';
+    }
+    public function create(Request $request){
+        return 'CREATE';
+    }
+    public function update(Request $request){
+        return 'UPDATE';
+    }
+    public function delete(Request $request){
+        return 'DELETE';
+    }
+}
