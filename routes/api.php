aa50705e99cf307c6fcfe55ecd2809b13ac3b5d6
adeputraprimasuhendri <adeputra.mtech@gmail.com>
2020-07-11 10:05:00 +0700

    ADD ID

--- routes/api.php
+++ routes/api.php
@@ -10,22 +10,22 @@ Route::get('product', 'ProductController@index')->middleware('jwt.verify');
 Route::get('product/{id}', 'ProductController@detail')->middleware('jwt.verify');
 Route::post('product/create', 'ProductController@create')->middleware('jwt.verify');
 Route::post('product/update', 'ProductController@update')->middleware('jwt.verify');
-Route::delete('product/delete', 'ProductController@delete')->middleware('jwt.verify');
+Route::delete('product/delete/{id}', 'ProductController@delete')->middleware('jwt.verify');
 
 Route::get('category', 'CategoryController@index')->middleware('jwt.verify');
 Route::get('category/{id}', 'CategoryController@detail')->middleware('jwt.verify');
 Route::post('category/create', 'CategoryController@create')->middleware('jwt.verify');
 Route::post('category/update', 'CategoryController@update')->middleware('jwt.verify');
-Route::delete('category/delete', 'CategoryController@delete')->middleware('jwt.verify');
+Route::delete('category/delete/{id}', 'CategoryController@delete')->middleware('jwt.verify');
 
 Route::get('brand', 'BrandController@index')->middleware('jwt.verify');
 Route::get('brand/{id}', 'BrandController@detail')->middleware('jwt.verify');
 Route::post('brand/create', 'BrandController@create')->middleware('jwt.verify');
 Route::post('brand/update', 'BrandController@update')->middleware('jwt.verify');
-Route::delete('brand/delete', 'BrandController@delete')->middleware('jwt.verify');
+Route::delete('brand/delete/{id}', 'BrandController@delete')->middleware('jwt.verify');
 
 Route::get('rack', 'RackController@index')->middleware('jwt.verify');
 Route::get('rack/{id}', 'RackController@detail')->middleware('jwt.verify');
 Route::post('rack/create', 'RackController@create')->middleware('jwt.verify');
 Route::post('rack/update', 'RackController@update')->middleware('jwt.verify');
-Route::delete('rack/delete', 'RackController@delete')->middleware('jwt.verify');
+Route::delete('rack/delete/{id}', 'RackController@delete')->middleware('jwt.verify');
